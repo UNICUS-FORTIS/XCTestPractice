e7b19ec45af8b2601d842ff522469e76c49c35d2
Louie Shin <unicus.fortis@gmail.com>
2024-04-19 21:33:49 +0900

    [Text] Nimble & Quick 을 활용한 유요하지 않은 테스트 케이스 추가

--- XCTestPracticeTests/ValidationNimbleTest.swift
+++ XCTestPracticeTests/ValidationNimbleTest.swift
@@ -32,12 +32,38 @@ final class ValidationNimbleTest: QuickSpec {
                     let validate = try sut.validateEmail(successEmail)
                     expect { validate }.to(equal(successEmail))
                 }
+                
+                it("유효하지 않은 이메일 검증") {
+                    let expectedError = ValidationError.invalidEmail
+                    var error: ValidationError?
+                    
+                    expect { try sut.validateEmail(failureEmail) }.to(throwError { error in
+                        guard let validationError = error as? ValidationError else { 
+                            fail("에러 확인 실패, \(error)")
+                            return
+                        }
+                        expect(validationError).to(equal(expectedError))
+                    })
+                }
             }
             
             context("비밀번호 유효성 검증") {
                 it("유효한 비밀번호 검증") {
                     let validate = try sut.validatePassword(successPassword)
                     expect { validate }.to(equal(successPassword))
+                }
+                
+                it("유효하지 않은 비밀번호 검증") {
+                    let expectedError = ValidationError.invalidPassword
+                    var error: ValidationError?
+                    
+                    expect { try sut.validatePassword(failurePassword) }.to(throwError { error in
+                        guard let validationError = error as? ValidationError else {
+                            fail("에러 확인 실패, \(error)")
+                            return
+                        }
+                        expect(validationError).to(equal(expectedError))
+                    })
                 }
             }
         }
